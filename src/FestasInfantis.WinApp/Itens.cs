b5a677ed48c3265a6d7923eab95305f37c5ce75b
chayannerech <rechchayanne@gmail.com>
2024-06-03 15:51:09 -0300

    Criado diagrama de classes

---
+++ src/FestasInfantis.WinApp/Itens.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FestasInfantis.WinApp
+{
+    public class Itens
+    {
+        public string Descricao
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Valor
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Tema Tema
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
